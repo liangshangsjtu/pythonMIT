1c44051499df41a17b6f410d4b4da2d9186abdb6
liangshangsjtu <41513648+liangshangsjtu@users.noreply.github.com>
2018-07-22 16:56:37 +0800

    Update file1.py

--- file1.py
+++ file1.py
@@ -21,3 +21,57 @@ for c in s:
         if s[i]=='o' and s[i+1]=='b':
             num += 1
 print('Number of times bob occurs is: '+str(num))
+
+"""
+GUESS-AND-CHECK IDEA TI FIND THE SQUARE ROOT OF AN INTEGER
+"""
+ans=0
+neg_flag=False
+x=int(input("Enter an interge here: ")) 
+if x<0:
+    neg_flag = True
+while ans ** 2 <x:
+    ans += 1
+if ans ** 2 = x:
+    print("square root of",x,"is",ans)
+else:
+    print(x,"is not a perfect square")
+    if neg_flag:
+        print("Just checking...did you mean",-x,"?")
+        
+ """
+ UPGRADE: FIND THE CUBE ROOT OF ANY NUMBER
+ """
+cube=27
+epsilon=0.01
+guess=0.0
+increment=0.001
+num_guesses=0
+while abs(guess**3-cube）>=epsilon:
+          guess += increment
+          num_guesses += 1
+ print("Number of guesses:",num_guesses)
+ if abs(guess**3-cube）>=epsilon:
+        print('failed on cube root of',cube)
+ else:
+        print(guess,'is close to the cube root of',cube)
+        
+# BISECTION SEARCH FOR SQUARE ROOT
+x=25
+epsilon=0.01
+numGuesses=0
+low=0.0
+high=x
+ans=(high+low)/2.0
+
+while abs(ans**2-x) >= epsilon:
+        print 'low=',low,'high=',high,'ans=',ans
+        numGuesses +=1
+        if ans**2<x:
+            low=ans
+        else:
+            high=ans
+        ans=(high+low)/2.0
+print 'numGuesses=',numGuesses
+print ans,"is close to the square root of",x
+       
