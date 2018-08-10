18762ed875a7130da55687e447c967ff7dd1de22
liangshangsjtu <41513648+liangshangsjtu@users.noreply.github.com>
2018-08-10 12:21:00 +0800

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,19 @@
 
+# FIND THE WORDS THE OCCUR MOST IN A SONG LYRICS
+def lyrics_to_frequencies(lyrics):
+  myDict={}
+  for word in lyrics:
+    if word in myDict:
+      myDict[word]+=1
+    else:
+      myDict[word]=1
+  return myDict
+#SO myDict IS THE freqs DICTIONARY
+def most_common_words(freqs):
+  values = freqs.values()
+  best=max(values)
+  word=[]
+  for k in freqs:
+    if freqs[k]==best:
+      words.append(k)
+  return (words, best)
